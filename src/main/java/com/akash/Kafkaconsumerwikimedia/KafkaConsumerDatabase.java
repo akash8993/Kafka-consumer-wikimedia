c278919afa055ed16d2bd2303c9d0731ab4f6f22
Akash Gupta <gr8akash1994@gmail.com>
2024-03-31 20:06:18 +0530

    Properties externalize

--- src/main/java/com/akash/Kafkaconsumerwikimedia/KafkaConsumerDatabase.java
+++ src/main/java/com/akash/Kafkaconsumerwikimedia/KafkaConsumerDatabase.java
@@ -5,17 +5,20 @@ import com.akash.Kafkaconsumerwikimedia.repository.WikimediaDataRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerDatabase {
 
+    @Value("${kafka.topic.name}")
+    private String topicName;
     private static final Logger logger= LoggerFactory.getLogger(KafkaConsumerDatabase.class);
 
     @Autowired
     private WikimediaDataRepo wikimediaDataRepo;
-    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "myGroup")
     public void consume(String eventMessage)
     {
         logger.info(String.format("Data recieved -> %s", eventMessage));
